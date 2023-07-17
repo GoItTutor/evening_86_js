a9f5f9567b5c3a6c09830f243015ab7bf85193a9
GoItTutor <sheva09a@gmail.com>
2023-07-17 20:40:33 +0200

    module-09-lesson-01

--- src/js/async.js
+++ src/js/async.js
@@ -8,4 +8,40 @@
 
 // Функції відкладеного виклику window.setTimeout(callback, delay, arg1, arg2, ...);
 
+// console.log(1);
+
+// setTimeout(() => {
+//   console.log(2);
+// }, 700);
+
+// setTimeout(() => {
+//   console.log(3);
+// }, 700);
+
+// console.log(4);
+
+// let seconds = 1;
+
+// setInterval(() => {
+//   console.log(seconds);
+
+//   seconds += 1;
+// }, 1000);
+
+// const ID = setTimeout(() => {
+//   console.log(`Hello!`);
+// }, 1000);
+
+// // console.log(ID);
+
+// setTimeout(() => clearTimeout(ID), 1001);
+
+// const ID = setInterval(() => {
+//   console.log(`Hello!`);
+// }, 1000);
+
+// // console.log(ID);
+
+// setTimeout(() => clearInterval(ID), 2999);
+
 // Очищення інтервалів і таймаутів clearInterval(intervalID), clearTimeout(timeoutID)
--- src/js/date.js
+++ src/js/date.js
@@ -1,19 +1,53 @@
 'use strict';
 // Створення нової дати
 
+// const date = new Date(1000);
+
+// console.dir(date);
+
 // Методи дат
+
+// const currentDate = new Date();
+
+// currentDate.setFullYear(2000);
+// currentDate.setMonth(1);
+
+// console.log(currentDate);
+// console.log(currentDate.getDate());
+// console.log(currentDate.getFullYear());
+// console.log(currentDate.getMonth());
 
 //? TASK 01
 // Створіть об'єкт Date для дати: 20 лютого 2012 року, 3 години 12 хвилин.
+
+// const date = new Date(1900, 1, 20, 3, 12);
+
+// console.log(date);
 
 //? TASK 02
 // Створіть функцію getWeekDay(date), яка показує день тижня у короткому форматі:
 // «ПН», «ВТ», «СР», «ЧТ», «ПТ», «СБ», «НД».
 
-//? TASK 03
-// Створіть функцію getLastDayOfMonth(year, month), яка повертає останнє число місяця.
-// Інколи це 30, 31 або навіть 28/29 у лютому.
-// Параметри:
-// year – рік з чотирьох цифр, наприклад, 2012.
-// month – місяць від 0 до 11.
-// Наприклад, getLastDayOfMonth(2012, 1) = 29 (високосний рік, лютий).
+// const weekDays = ['НД', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ'];
+
+// const getWeekDay = date => {
+//   console.log(weekDays[date.getDay()]);
+// };
+
+// getWeekDay(new Date(2023, 6, 18));
+
+// const button = document.querySelector('button');
+
+// button.addEventListener('click', () => {
+//   setTimeout(() => {
+//     console.log('loh');
+//   }, 3000);
+// });
+
+const obj = {
+  //   color: { hex: '#000', regular: 'black' },
+  friends: [],
+  isAdmin: false,
+};
+
+console.log(obj?.color?.hex);
